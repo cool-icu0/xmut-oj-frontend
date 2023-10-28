68ea01ec1e553a35263626c8ea8d5f5d70b7491e
Cool <990299103@qq.com>
2023-10-29 00:21:13 +0800

    修改基础样式，设置layout格式

---
+++ src/layouts/BasicLayout.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="BasicLayout">
+    <a-layout style="height: 400px">
+      <a-layout-header class="header">导航栏</a-layout-header>
+      <a-layout-content class="content">
+        <router-view />
+      </a-layout-content>
+      <a-layout-footer class="footer">
+        <a href="https://yuque.com/icu0" target="_blank">
+          厦门理工学院在线判题系统 By Cool
+        </a>
+      </a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+
+<style scoped>
+#BasicLayout {
+}
+
+#BasicLayout .header {
+  background: red;
+  margin-bottom: 16px;
+}
+
+#BasicLayout .content {
+  background: linear-gradient(to right, #bbb, #fff);
+  margin-bottom: 16px;
+}
+
+#BasicLayout .footer {
+  background: #efefef;
+}
+</style>
